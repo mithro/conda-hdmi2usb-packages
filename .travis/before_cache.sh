12ad0a52849a8f8ace7b120b5de63b98c857a43f
Tim 'mithro' Ansell <me@mith.ro>
2019-02-07 20:36:10 +1100

    Trying to fix install script.
    Signed-off-by: Tim 'mithro' Ansell <me@mith.ro>

--- .travis/before_cache.sh
+++ .travis/before_cache.sh
@@ -7,13 +7,13 @@ set -e
 travis_fold end before_cache
 
 start_section "conda.clean.1" "${GREEN}Clean status...${NC}"
-conda clean -s --dry-run
+#conda clean -s --dry-run
 end_section "conda.clean.1"
 
 start_section "conda.clean.2" "${GREEN}Cleaning...${NC}"
-conda build purge
+#conda build purge
 end_section "conda.clean.2"
 
 start_section "conda.clean.3" "${GREEN}Clean status...${NC}"
-conda clean -s --dry-run
+#conda clean -s --dry-run
 end_section "conda.clean.3"
