098dae47b63b85091a5acf85e4225007eea3c795
Tim 'mithro' Ansell <me@mith.ro>
2019-04-15 16:52:57 -0700

    vtr: Keep going and then try again.
    Signed-off-by: Tim 'mithro' Ansell <me@mith.ro>

--- vtr/build.sh
+++ vtr/build.sh
@@ -35,7 +35,8 @@ mkdir build
 cd build
 cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=${PREFIX} ..
 grep -i flags CMakeCache.txt
-make -j$CPU_COUNT || make VERBOSE=1
+make -k -j$CPU_COUNT || make VERBOSE=1
+make VERBOSE=1
 make test
 make install
 
