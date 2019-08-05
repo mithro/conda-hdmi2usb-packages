d9d1742509bd14b36df71cdf1abdfdd1949c0f36
Keith Rothman <537074+litghost@users.noreply.github.com>
2019-08-05 14:50:18 -0700

    Force install libiconv to prevent conda from breaking itself.
    Signed-off-by: Keith Rothman <537074+litghost@users.noreply.github.com>

--- conda-get.sh
+++ conda-get.sh
@@ -11,6 +11,11 @@ if [ ! -d $CONDA_PATH -o ! -z "$CI"  ]; then
         ./Miniconda3-latest-Linux-x86_64.sh -p $CONDA_PATH -b -f
 fi
 export PATH=$CONDA_PATH/bin:$PATH
+
+# This appears to be required now because of a broken upstream package
+# dependency in conda-forge.
+conda install -y libiconv
+
 conda update -y conda
 if [ ! -z "$CONDA_BUILD_VERSION" ]; then
 	conda install -y conda-build==$CONDA_BUILD_VERSION
