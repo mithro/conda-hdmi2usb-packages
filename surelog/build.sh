8796eeaaf4da531613a3b23a69e83bbe5d9799ca
Henner Zeller <h.zeller@acm.org>
2019-11-19 18:40:47 -0800

    Add surelog conda package.
    Signed-off-by: Henner Zeller <h.zeller@acm.org>
    Signed-off-by: Tim 'mithro' Ansell <me@mith.ro>

---
+++ surelog/build.sh
@@ -0,0 +1,16 @@
+#! /bin/bash
+
+set -e
+set -x
+
+if [ x"$TRAVIS" = xtrue ]; then
+  CPU_COUNT=2
+fi
+
+mkdir build && cd build
+
+cmake .. -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=${PREFIX} -DCMAKE_VERBOSE_MAKEFILE=1
+
+make -j$CPU_COUNT install
+
+
