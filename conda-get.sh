7a906228ffe4a48c17e768d52eb2f71b45a3700e
Tim 'mithro' Ansell <me@mith.ro>
2019-11-21 15:46:23 -0800

    Cleanup the conda-get.sh script.
    Signed-off-by: Tim 'mithro' Ansell <me@mith.ro>

--- conda-get.sh
+++ conda-get.sh
@@ -5,23 +5,16 @@ set -e
 
 CONDA_PATH=${1:-~/conda}
 
+echo "Downloading Conda installer."
 wget -c https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh
 chmod a+x Miniconda3-latest-Linux-x86_64.sh
+
 if [ ! -d $CONDA_PATH -o ! -z "$CI"  ]; then
+	echo "Installing conda"
         ./Miniconda3-latest-Linux-x86_64.sh -p $CONDA_PATH -b -f
 fi
 export PATH=$CONDA_PATH/bin:$PATH
 
-# This appears to be required now because of a broken upstream package
-# dependency in conda-forge.
-conda install -y libiconv
-
+# Updating conda
 conda update -y conda setuptools
-if [ ! -z "$CONDA_BUILD_VERSION" ]; then
-	conda install -y conda-build==$CONDA_BUILD_VERSION
-	echo "conda-build==$CONDA_BUILD_VERSION" > $CONDA_PATH/conda-meta/pinned
-else
-	conda install -y conda-build
-fi
-conda install -y anaconda-client
-conda install -y jinja2
+conda install -y conda-build anaconda-client jinja2 conda-verify ripgrep
