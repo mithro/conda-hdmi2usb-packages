932dd53623118f172577f89c6a681ca23c3f8cbe
Tim 'mithro' Ansell <me@mith.ro>
2019-11-21 17:40:56 -0800

    iverilog: Echo commands as being run.
    Signed-off-by: Tim 'mithro' Ansell <me@mith.ro>

--- iverilog/run_test.sh
+++ iverilog/run_test.sh
@@ -12,6 +12,7 @@ cd test
 echo
 echo
 echo "Hello World Test ====="
+echo $PREFIX/bin/iverilog -v hello_world.v -o hello_world
 $PREFIX/bin/iverilog -v hello_world.v -o hello_world
 echo "----------------------"
 cat hello_world
@@ -25,14 +26,18 @@ echo "----------------------"
 echo
 echo
 echo "Counter Test ========="
+echo iverilog -o test_counter counter_tb.v counter.v
 iverilog -o test_counter counter_tb.v counter.v
 echo "----------------------"
 cat test_counter
 echo "- - - - - - - - - - --"
+echo vvp -n test_counter
 vvp -n test_counter
 echo "----------------------"
+echo iverilog -o test_counter -c counter_list.txt
 iverilog -o test_counter -c counter_list.txt
 echo "- - - - - - - - - - --"
+echo vvp -n test_counter
 vvp -n test_counter
 echo "----------------------"
 
@@ -40,6 +45,7 @@ echo "----------------------"
 echo
 echo
 echo "FSM Test ============="
+echo iverilog -o test_fsm fsm.v
 iverilog -o test_fsm fsm.v
 echo "----------------------"
 cat test_fsm
